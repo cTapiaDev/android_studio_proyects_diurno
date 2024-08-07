e5688035d0136fd2afd56987e5e4b490b7c09126
Carlos Tapia <ctapiadev@gmail.com>
2024-08-07 09:57:37 -0400

    clase 5 on click

--- modulo04/clase4/app/src/main/java/cl/bootcamp/interfazgrafica/MainActivity.java
+++ modulo04/clase4/app/src/main/java/cl/bootcamp/interfazgrafica/MainActivity.java
@@ -1,6 +1,7 @@
 package cl.bootcamp.interfazgrafica;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+
+
     }
 }
