b8c57105b3f752e39dda1a900bae9e0ec845aaa1
Carlos Tapia <ctapiadev@gmail.com>
2024-09-04 10:46:00 -0400

    desing on boarding

---
+++ modulo_05/app_on_boarding/app/src/main/java/cl/bootcamp/apponboarding/MainActivity.kt
@@ -0,0 +1,29 @@
+package cl.bootcamp.apponboarding
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import cl.bootcamp.apponboarding.onBoardingViews.MainOnBoarding
+import cl.bootcamp.apponboarding.ui.theme.AppOnBoardingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+        setContent {
+            AppOnBoardingTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainOnBoarding(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
